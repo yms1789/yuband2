5beecee060f9010abc8657f03fe650ffd2bfc197
YMS <yms1789@naver.com>
2021-11-28 00:58:43 +0900

    yuband

---
+++ src/test/java/yasu/yuband/YubandApplicationTests.java
@@ -0,0 +1,13 @@
+package yasu.yuband;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YubandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
